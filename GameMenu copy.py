251d5a02fbcfc03f0ce5e15835b951f29f30b1e3
jjjghu <chiuliyou@gmail.com>
2025-03-26 18:08:40 +0800

    UI更改為pack

--- GameMenu copy.py
+++ GameMenu copy.py
@@ -27,6 +27,14 @@ class GameMenu:
         self.window.geometry(f"{w}x{h}+{x}+{y}")
         self.window.configure(bg="BLACK")
         self.window.resizable(False, False)
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
+        
+    def griding(self, frame, rows, cols):
+        for i in range(rows):
+            frame.grid_rowconfigure(i, weight=1)
+        for i in range(cols):
+            frame.grid_columnconfigure(i, weight=1)
     
     def enter_games(self, gameType : str):
         self.window.withdraw()
@@ -43,26 +51,28 @@ class GameMenu:
         self.window.deiconify()
     
     def create_menu(self):
-        self.menu_frame = Frame(self.window, bg="black", highlightthickness=2, highlightbackground="white")
-        self.menu_frame.pack(expand=True, padx=40, pady=40, fill=BOTH)
+        self.menu_frame = Frame(self.window, bg="black")
+        self.menu_frame.grid(row=0, column=0, sticky="nsew")
+        self.griding(self.menu_frame,8, 5)
         
-        # 標題
         title_label = Label(self.menu_frame, text="任天堂最新版", font=("Arial", 30), bg="black", fg="white")
-        title_label.pack(pady=20)
+        title_label.grid(row=1, column=2, padx=20, pady=20, sticky="NSEW")
         
         # 遊戲按鈕
+        row = 2
         gameTypes = [MONTY_HALL, TIC_TAC_TOE, MINESWEEPER]
         for gameType in gameTypes:
             button = Button(self.menu_frame, text=gameType, font=("Arial", 14),
                             bg="lightblue", activebackground="lightblue",
-                            command=lambda gameType=gameType: self.enter_games(gameType))
-            button.pack(padx=50, pady=30, ipady=30, fill=X)
+                            command = lambda gameType=gameType: self.enter_games(gameType))
+            button.grid(row=row,column=2,padx=50,pady=20,sticky=NSEW)
+            row += 1
         
         # 退出按鈕
         exit_btn = Button(self.menu_frame, text="退出", font=("Arial", 14),
                           bg="red", activebackground="darkred",
                           command=self.window.quit)
-        exit_btn.pack(padx=50, pady=30, ipady=30, fill=X)
+        exit_btn.grid(row=row, column=2, padx=50, pady=20, sticky="NSEW")
         
 if __name__ == "__main__":
     gamemenu = GameMenu()
